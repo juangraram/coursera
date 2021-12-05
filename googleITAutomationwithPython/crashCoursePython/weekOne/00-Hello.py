4153c2b4840e72d57960d4468d9611caeb732a6f
juangraram <juangraram@zohomail.com>
2021-12-05 14:37:07 -0500

    Examples of code

---
+++ googleITAutomationwithPython/crashCoursePython/weekOne/00-Hello.py
@@ -0,0 +1,29 @@
+# Print and show on the screen
+print("My first Python program")
+
+# through the list and print the values
+friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+for friend in friends:
+    print("Hi " + friend)
+
+# Print number from 1 to 10
+for i in range(10):
+  print("Hello, World!")
+
+# Fill in the correct Python commands to put “This is fun!” onto the screen 5 times.
+for i in range(5):
+  print("This is fun!")
+
+# Getting information from the user
+name = "Brook"
+print('Hello' + name)
+
+color = "Red"
+thing = "Love"
+print(color + " is the color of " + thing)
+
+# Use Python to calculate (((1+2)*3)/4)^5
+print((((1+2)*3)/4)**5)
+
+
+
